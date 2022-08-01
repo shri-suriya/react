5219b9c5bcb923f387261dd2aad403641b19c1c0
shri-suriya <shrisuriya23@gmail.com>
2022-08-01 11:21:28 +0530

    Fetched data from API

--- src/component/Section/Section.jsx
+++ src/component/Section/Section.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 
 import './Section.css'
 
 function Section() {
+    const EmpUrl='https://randomuser.me/api/?results=10';
+    const[data,setData]=useState([]);
+    useEffect(() => {
+        fetch(EmpUrl).then((response)=>{
+            response.json().then((data)=>{
+                console.log(data)
+                 setData(data);
+            })
+        })
+    },[])
+
   return (
-    <div>Section</div>
+    <div></div>
   )
 }
 
