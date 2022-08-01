50a01155ed1e9548c8a793463b69d6415273f831
shri-suriya <shrisuriya23@gmail.com>
2022-08-01 10:27:23 +0530

    Added Nav bar details

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <div class="topnav">
+      <h1>EMPLOYEE DETAILS</h1>
+      <a href="#">Home</a>
+      <a href="#">Details</a>
+      <a href="#">Contact</a>
+      <a href="#">About</a>
+      <a href="#">Login</a>
+  </div>
+    </div>
+  )
+}
+
+export default Navbar
